2aa2f3c847c0c9d907b27427f3aa7e3c76f8f581
Wade Cline <wadecline@hotmail.com>
2018-07-02 19:03:07 -0700

    Add tests for peer cert chain logging

--- ssl_log.sh
+++ ssl_log.sh
@@ -3,15 +3,26 @@
 # Test InspIRCd OpenSSL logging functionality.
 
 # Test 01: Valid certificate
+#   A -> B (Server has root CA 'A' and client connects using signed
+#   cert 'B').
 # Test 02: Invalid certificate
+#   A,  D -> E (Server has root CA 'A' and client connects using cert 'E'
+#   which is signed by untrusted root CA 'D').
 # Test 03: Missing certificate
+#   Client presents no certificate.
+# Test 04: Server uses root and intermediate CA
+#   A -> B -> C (Server trusts root CA 'A' and intermediate CA 'B', client
+#   presents signed cert 'C')
+# Test 05: Client presents intermediate CA and leaf cert
+#   A -> B -> C (Server trusts root CA 'A' and client connects using
+#   intermediate CA 'B' and signed cert 'C')
 
 # Load includes.
 source include.sh
 
 # Restore original configuration.
 restore() {
-	for name in root_ca leaf_ca fake_ca fail_ca; do
+	for name in root_ca leaf_ca fake_ca fail_ca a b c; do
 		rm -rf "${DIR_HOME}/${name}"
 	done
 	rm -rf "${DIR_SSL}"
@@ -24,9 +35,11 @@ restore() {
 
 # Connect to the server.
 # $1 User to connect.
+# $2 Additional cert to connect with.
 connect() {
 	local name="$1"
-	local output=$( (echo -e "USER a hostess servant rjhacker\nNICK a"; sleep 5; echo "QUIT") | openssl s_client -verify_return_error -CAfile "${DIR_SSL}/certs/cert.pem" -cert "${name}/certs/${name}.pem" -key "${name}/private/${name}.pem" -connect 127.0.0.1:6697 -ign_eof)
+	local chain="${2:+-cert_chain $2/certs/$2.pem}"
+	local output=$( (echo -e "USER a hostess servant rjhacker\nNICK a"; sleep 5; echo "QUIT") | openssl s_client -verify_return_error -CAfile "${DIR_SSL}/certs/cert.pem" -cert "${name}/certs/${name}.pem" -key "${name}/private/${name}.pem" ${chain} -connect 127.0.0.1:6697 -ign_eof)
 }
 
 # Sanity check for log 'flush=1'.
@@ -77,12 +90,12 @@ else
 	TEST01=0
 fi
 
-# Generate invalid client certificate
+# Generate invalid root certificate
 ca_gen "fake_ca"
 ca_selfsign "fake_ca"
 ca_crl_gen "fake_ca"
 
-# Generate valid client certificate
+# Generate invalid client certificate
 ca_gen "fail_ca"
 ca_req_gen "fail_ca"
 cp "fail_ca/csr/fail_ca.pem" "fake_ca/csr/fail_ca.pem"
@@ -119,6 +132,81 @@ else
 	TEST03=0
 fi
 
+# Setup chain for tests 04 and 05.
+# A
+ca_gen "a"
+ca_selfsign "a"
+ca_crl_gen "a"
+# B
+ca_gen "b"
+ca_req_gen "b"
+cp "b/csr/b.pem" "a/csr/b.pem"
+ca_req_sign "a" "b"
+cp "a/certs/b.pem" "b/certs/b.pem"
+ca_crl_gen "b"
+# C
+ca_gen "c"
+ca_req_gen "c"
+cp "c/csr/c.pem" "b/csr/c.pem"
+ca_req_sign "b" "c"
+cp "b/certs/c.pem" "c/certs/c.pem"
+
+# Test 04: Server uses root and intermediate CAs.
+echo "Test 04:"
+## Configure server.
+cp "a/certs/a.pem" "${DIR_SSL}/certs/cert.pem"
+cat "a/certs/a.pem" "b/certs/b.pem" > "${DIR_SSL}/certs/client_cas.pem"
+cp "a/private/a.pem" "${DIR_SSL}/private/key.pem"
+cat "a/crl/a.pem" "b/crl/b.pem" > "${DIR_SSL}/crl/crl.pem"
+rm "${LOG}"
+rc-service inspircd restart
+## Connect to server.
+connect "c"
+## Check IRCd log.
+fingerprint=$(openssl x509 -in "c/certs/c.pem" -fingerprint -sha256 -noout | cut -d '=' -f 2 | sed 's/://g' | sed 'y/ABCDEF/abcdef/')
+if cat ${LOG} | grep "${fingerprint}"; then
+	echo "Fingerprint found"
+	TEST04=1
+else
+	echo "Fingerprint not found: $(cat ${LOG})"
+	TEST04=0
+fi
+
+# Test 05: Client presents multiple certificates.
+# FIXME: This fails unless the server already has a CRL for 'B', is there a way
+# for the client to pass B's CRL?
+echo "Test 05:"
+## Configure server.
+cp "a/certs/a.pem" "${DIR_SSL}/certs/cert.pem"
+cp "a/certs/a.pem" "${DIR_SSL}/certs/client_cas.pem"
+cp "a/private/a.pem" "${DIR_SSL}/private/key.pem"
+cat "a/crl/a.pem" "b/crl/b.pem" > "${DIR_SSL}/crl/crl.pem"
+rm "${LOG}"
+rc-service inspircd restart
+## Connect to server.
+connect "c" "b"
+## Check IRCd log.
+fingerprint_b=$(openssl x509 -in "b/certs/b.pem" -fingerprint -sha256 -noout | cut -d '=' -f 2 | sed 's/://g' | sed 'y/ABCDEF/abcdef/')
+fingerprint_c=$(openssl x509 -in "c/certs/c.pem" -fingerprint -sha256 -noout | cut -d '=' -f 2 | sed 's/://g' | sed 'y/ABCDEF/abcdef/')
+if cat ${LOG} | grep "${fingerprint_b}"; then
+	echo "Fingerprint for b found"
+else
+	echo "Fingerprint b not found ($fingerprint_b)"
+	fingerprint_b=""
+fi
+if cat ${LOG} | grep "${fingerprint_c}"; then
+	echo "Fingerprint for c found"
+else
+	echo "Fingerprint c not found ($fingerprint_c)"
+	fingerprint_c=""
+fi
+if [ -z "${fingerprint_b}" -o -z "${fingerprint_c}" ]; then
+	echo "Log: $(cat ${LOG})"
+	TEST05=0
+else
+	TEST05=1
+fi
+
 # Report results.
 PASS=1
 if [ $TEST01 -eq 1 ]; then
@@ -138,6 +226,18 @@ if [ $TEST03 -eq 1 ]; then
 else
 	PASS=0
 	echo "Test 03 FAILED"
+fi
+if [ $TEST04 -eq 1 ]; then
+	echo "Test 04 PASSED!"
+else
+	PASS=0
+	echo "Test 04 FAILED"
+fi
+if [ $TEST05 -eq 1 ]; then
+	echo "Test 05 PASSED!"
+else
+	PASS=0
+	echo "Test 05 FAILED"
 fi
 
 restore
