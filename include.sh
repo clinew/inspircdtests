32125373003f539fd83a67f5a562612e94512afc
Wade Cline <wadecline@hotmail.com>
2019-10-06 14:48:04 -0700

    Add license

--- include.sh
+++ include.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Various helper functions
+# Copyright (C) 2018  Wade T. Cline
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # Test dir home to backup original config.
 DIR_HOME="${HOME}/.inspircdtests"
 # Home dir for the InspIRCd configuration.
