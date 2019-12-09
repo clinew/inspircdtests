a099300df9dbcd18cc601b4519538fbc152fa6bd
Wade Cline <wadecline@hotmail.com>
2019-12-08 21:42:16 -0800

    Remove DSA tests from strongcert tests
    DSA support removed from TLS1.3, so might as well remove it from the
    tests, too.

--- ssl_strongcert.sh
+++ ssl_strongcert.sh
@@ -89,10 +89,6 @@ ca_selfsign "root_ca"
 openssl genrsa -out rsa-8k.pem 8192
 openssl genrsa -out rsa-4k.pem 4096
 openssl genrsa -out rsa-2k.pem 2048
-openssl dsaparam -out dsaparam-4k.pem 4096
-openssl gendsa -out dsa-4k.pem dsaparam-4k.pem
-openssl dsaparam -out dsaparam-2k.pem 2048
-openssl gendsa -out dsa-2k.pem dsaparam-2k.pem
 openssl ecparam -out ec.pem -name brainpoolP512r1 -genkey
 openssl genrsa -out intr-4k.pem 4096
 openssl genrsa -out intr-2k.pem 2048
@@ -121,14 +117,6 @@ ca_req_sign "root_ca" "rsa-2k-sha256" "" "sha256"
 openssl req -new -key rsa-4k.pem -out req.pem -subj "/CN=RSA-4k-SHA1/"
 mv "req.pem" "root_ca/csr/rsa-4k-sha1.pem"
 ca_req_sign "root_ca" "rsa-4k-sha1" "" "sha1"
-# 4k-DSA + SHA512
-openssl req -new -key dsa-4k.pem -out req.pem -subj "/CN=DSA-4k-SHA512/"
-mv "req.pem" "root_ca/csr/dsa-4k-sha512.pem"
-ca_req_sign "root_ca" "dsa-4k-sha512" "" "sha512"
-# 2k-DSA + SHA512
-openssl req -new -key dsa-2k.pem -out req.pem -subj "/CN=DSA-2k-SHA512/"
-mv "req.pem" "root_ca/csr/dsa-2k-sha512.pem"
-ca_req_sign "root_ca" "dsa-2k-sha512" "" "sha512"
 # EC + SHA512
 openssl req -new -key ec.pem -out req.pem -subj "/CN=EC-SHA512/"
 mv "req.pem" "root_ca/csr/ec-sha512.pem"
@@ -228,84 +216,89 @@ if [ $? -eq 1 ]; then
 else
 	results[5]="FAILED"
 fi
+# Test 06: EC 256 fails
+echo "Test 06"
+cli_con_test "ec-sha512" "ec.pem"
+if [ $? -eq 1 ]; then
+	results[6]=""
+else
+	results[6]="FAILED"
+fi
 
 ### Multi cert/alg tests
 ## Sigalg SHA512 + SHA256 test
 sed -ri "s/(peer_keysize_min)/#\1/" "${DIR_IRCD}/modules.conf"
 sed -ri "s/#*(peer_sigalg=\")[^\"]+/\1RSA-SHA512,RSA-SHA256/" "${DIR_IRCD}/modules.conf"
 rc-service inspircd restart
-# Test 06: 4k RSA, SHA512 passes
-echo "Test 06"
-cli_con_test "rsa-4k-sha512" "rsa-4k.pem"
-if [ $? -eq 0 ]; then
-	results[6]=""
-else
-	results[6]="FAILED"
-fi
-# Test 07: 4k RSA, SHA256 passes
+# Test 07: 4k RSA, SHA512 passes
 echo "Test 07"
-cli_con_test "rsa-4k-sha256" "rsa-4k.pem"
+cli_con_test "rsa-4k-sha512" "rsa-4k.pem"
 if [ $? -eq 0 ]; then
 	results[7]=""
 else
 	results[7]="FAILED"
 fi
-# Test 08: 4k RSA, SHA1 fails
+# Test 08: 4k RSA, SHA256 passes
 echo "Test 08"
-cli_con_test "rsa-4k-sha1" "rsa-4k.pem"
-if [ $? -eq 1 ]; then
+cli_con_test "rsa-4k-sha256" "rsa-4k.pem"
+if [ $? -eq 0 ]; then
 	results[8]=""
 else
 	results[8]="FAILED"
 fi
-## RSA + DSA test
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:4096,dsaEncryption:4096/" "${DIR_IRCD}/modules.conf"
-sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
-rc-service inspircd restart
-# Test 09: RSA 4096 passes
+# Test 09: 4k RSA, SHA1 fails
 echo "Test 09"
-cli_con_test "rsa-4k-sha512" "rsa-4k.pem"
-if [ $? -eq 0 ]; then
+cli_con_test "rsa-4k-sha1" "rsa-4k.pem"
+if [ $? -eq 1 ]; then
 	results[9]=""
 else
 	results[9]="FAILED"
 fi
-# Test 10: RSA 2048 fails
+
+### Parsing tests
+# Test 10: Bogus key type test
 echo "Test 10"
-cli_con_test "rsa-2k-sha512" "rsa-2k.pem"
-if [ $? -eq 1 ]; then
+sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1fakeKeyAlg:4096/" "${DIR_IRCD}/modules.conf"
+sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
+rc-service inspircd restart
+if [ $? -ne 0 ]; then
 	results[10]=""
 else
 	results[10]="FAILED"
 fi
-# Test 11: DSA 4096 passes
+# Test 11: Zero key size
 echo "Test 11"
-cli_con_test "dsa-4k-sha512" "dsa-4k.pem"
-if [ $? -eq 0 ]; then
+sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:0/" "${DIR_IRCD}/modules.conf"
+sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
+rc-service inspircd restart
+if [ $? -ne 0 ]; then
 	results[11]=""
 else
 	results[11]="FAILED"
 fi
-# Test 12: DSA 2048 fails
+# Test 12: Negative key size
 echo "Test 12"
-cli_con_test "dsa-2k-sha512" "dsa-2k.pem"
-if [ $? -eq 1 ]; then
+sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:-4096/" "${DIR_IRCD}/modules.conf"
+sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
+rc-service inspircd restart
+if [ $? -ne 0 ]; then
 	results[12]=""
 else
 	results[12]="FAILED"
 fi
-# Test 13: EC 256 fails
+# Test 13: Bogus key size value
 echo "Test 13"
-cli_con_test "ec-sha512" "ec.pem"
-if [ $? -eq 1 ]; then
+sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:FooBar/" "${DIR_IRCD}/modules.conf"
+sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
+rc-service inspircd restart
+if [ $? -ne 0 ]; then
 	results[13]=""
 else
 	results[13]="FAILED"
 fi
-
-### Parsing tests
-# Test 14: Bogus key type test
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1fakeKeyAlg:4096/" "${DIR_IRCD}/modules.conf"
+# Test 14: Multiple ':' delimiters in key size
+echo "Test 14"
+sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:2048:4096/" "${DIR_IRCD}/modules.conf"
 sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
 rc-service inspircd restart
 if [ $? -ne 0 ]; then
@@ -313,91 +306,57 @@ if [ $? -ne 0 ]; then
 else
 	results[14]="FAILED"
 fi
-# Test 15: Zero key size
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:0/" "${DIR_IRCD}/modules.conf"
-sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
+# Test 15: Bogus algorithm
+echo "Test 15"
+sed -ri "s/(peer_keysize_min)/#\1/" "${DIR_IRCD}/modules.conf"
+sed -ri "s/#*(peer_sigalg=\")[^\"]+/\1FakeSignatureAlgorithm/" "${DIR_IRCD}/modules.conf"
 rc-service inspircd restart
 if [ $? -ne 0 ]; then
 	results[15]=""
 else
 	results[15]="FAILED"
 fi
-# Test 16: Negative key size
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:-4096/" "${DIR_IRCD}/modules.conf"
+# Test 16: Same key specified multiple times
+echo "Test 16"
+sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:4096,rsaEncryption:2048/" "${DIR_IRCD}/modules.conf"
 sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
+grep peer "${DIR_IRCD}/modules.conf"
 rc-service inspircd restart
 if [ $? -ne 0 ]; then
 	results[16]=""
 else
 	results[16]="FAILED"
 fi
-# Test 17: Bogus key size value
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:FooBar/" "${DIR_IRCD}/modules.conf"
-sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
-rc-service inspircd restart
-if [ $? -ne 0 ]; then
-	results[17]=""
-else
-	results[17]="FAILED"
-fi
-# Test 18: Multiple ':' delimiters in key size
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:2048:4096/" "${DIR_IRCD}/modules.conf"
-sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
-rc-service inspircd restart
-if [ $? -ne 0 ]; then
-	results[18]=""
-else
-	results[18]="FAILED"
-fi
-# Test 19: Bogus algorithm
-sed -ri "s/(peer_keysize_min)/#\1/" "${DIR_IRCD}/modules.conf"
-sed -ri "s/#*(peer_sigalg=\")[^\"]+/\1FakeSignatureAlgorithm/" "${DIR_IRCD}/modules.conf"
-rc-service inspircd restart
-if [ $? -ne 0 ]; then
-	results[19]=""
-else
-	results[19]="FAILED"
-fi
-# Test 20: Same key specified multiple times
-sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:4096,rsaEncryption:2048/" "${DIR_IRCD}/modules.conf"
-sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
-grep peer "${DIR_IRCD}/modules.conf"
-rc-service inspircd restart
-if [ $? -ne 0 ]; then
-	results[20]=""
-else
-	results[20]="FAILED"
-fi
 
 ### Peer chain tests.
 sed -ri "s/#*(peer_keysize_min=\")[^\"]+/\1rsaEncryption:4096/" "${DIR_IRCD}/modules.conf"
 sed -ri "s/(peer_sigalg)/#\1/" "${DIR_IRCD}/modules.conf"
 rc-service inspircd restart
-# Test 21: 4k intermediate, 4k leaf passes
-echo "Test 21"
+# Test 17: 4k intermediate, 4k leaf passes
+echo "Test 17"
 cli_con_test "intr-4k-leaf-4k" "rsa-4k.pem" "intr_4k" "intr_4k/certs/intr_4k.pem"
 if [ $? -eq 0 ]; then
-	results[21]=""
+	results[17]=""
 else
-	results[21]="FAILED"
+	results[17]="FAILED"
 fi
-# Test 22: 4k intermediate, 2k leaf fails
-echo "Test 22"
+# Test 18: 4k intermediate, 2k leaf fails
+echo "Test 18"
 rc-service inspircd restart
 cli_con_test "intr-4k-leaf-2k" "rsa-2k.pem" "intr_4k" "intr_4k/certs/intr_4k.pem"
 if [ $? -eq 1 ]; then
-	results[22]=""
+	results[18]=""
 else
-	results[22]="FAILED"
+	results[18]="FAILED"
 fi
-# Test 23: 2k intermediate, 4k leaf fails
-echo "Test 23"
+# Test 19: 2k intermediate, 4k leaf fails
+echo "Test 19"
 rc-service inspircd restart
 cli_con_test "intr-2k-leaf-4k" "rsa-4k.pem" "intr_2k" "intr_2k/certs/intr_2k.pem"
 if [ $? -eq 1 ]; then
-	results[23]=""
+	results[19]=""
 else
-	results[23]="FAILED"
+	results[19]="FAILED"
 fi
 
 # Print results.
